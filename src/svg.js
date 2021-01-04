a416863ccd92b9c38120fdffad9108e031e12577
manfred <webdesign@milchrahm.at>
2021-01-04 18:43:33 +0100

    svg work, mainly card layout of player's own cards

--- src/svg.js
+++ src/svg.js
@@ -1,5 +1,6 @@
 const xmlns = "http://www.w3.org/2000/svg";
 
+
 function  alignSvgObject(object) {
     var bbox = object.getBBox();
     var width = bbox.width;
@@ -22,6 +23,7 @@ class SvgPlayer {
             //BALANCE
             var balanceGroup = document.createElementNS(xmlns, 'g');
             var balance = document.createElementNS(xmlns, 'text');
+            balance.classList.add("text-white");
             var balanceText = document.createTextNode( "$ " + this.balance );
             balance.appendChild(balanceText);
             balanceGroup.appendChild(balance);
@@ -86,6 +88,10 @@ class SvgPlayer {
             element.setAttributeNS(null, 'transform', 'translate('+ ( boxWidth/2 - width/2 ) + ' ' + (boxHeight/2 + height/4 + addTranslateY) +')' );
 
             alignSvgObject(nameGroup);   //has to be called after the element to align has already been rendered
+
+
+            parent.removeChild(balanceGroup);
+            parent.appendChild(balanceGroup);
         }
     }
 }
@@ -126,13 +132,84 @@ class OtherPlayer extends SvgPlayer {
     }
 }
 
+class SelfPlayer extends SvgPlayer {
+
+    constructor(name, balance, card0 = null, card1 = null){
+        console.log("SelfPlayer: ", name, balance, card0, card1);
+
+        super(name, balance);
+
+        this.card0 = card0;
+        this.card1 = card1;
+    }
+
+    getCardGroup() {
+        this.cardGroup = document.createElementNS(xmlns, "g");
+
+        var card1 = this.createCard(this.card0, -15, -3.5); // -2 because half width of card, -1.5 because cards should be 3 apart
+        this.cardGroup.append(card1);
+
+        var card2 = this.createCard(this.card1, 15, -0.5);  // -2 because half width of card, +1.5 because cards should be 3 apart
+        this.cardGroup.append(card2);
+
+        return this.cardGroup;
+    }
+
+    createCard(cardObject = this.card0, rotation = 15, translationX = 0) {
+        var  height = 7;
+        var width = 4;
+
+        var cardGroup = document.createElementNS(xmlns, "g");
+        cardGroup.setAttributeNS(null, 'transform', 'translate('+ translationX +' -4) rotate('+rotation+ ')');
+
+
+        if(cardObject) {
+            var card = document.createElementNS(xmlns, "rect");
+            card.setAttributeNS(null, 'width', width);
+            card.setAttributeNS(null, 'height', height);
+            card.setAttributeNS(null, 'ry', '2%');
+
+            card.setAttributeNS(null, 'class', 'card card-front');
+            cardGroup.append(card);
+
+            //SUIT
+            var suit = null;
+            console.log(cardObject);
+            switch(cardObject.suit) {
+                case 'c':   suit = SvgSuit.getSvg(SvgSuit.clubs);
+                            break;
+                case 'h':   suit = SvgSuit.getSvg(SvgSuit.hearts);
+                            break;
+                case 'd':   suit = SvgSuit.getSvg(SvgSuit.diamonds);
+                            break;
+                case 's':   suit = SvgSuit.getSvg(SvgSuit.spades);
+                            break;
+            }
+
+            if(suit) {
+                suit.setAttributeNS(null, 'transform', 'translate(-2 -1.3) scale(0.01)');
+                cardGroup.append(suit);
+            }
+
+            //VALUE
+            var cardValueSvg = document.createElementNS(xmlns, 'text');
+            var cardValueSvgText = document.createTextNode( cardObject.value );
+            cardValueSvg.setAttributeNS(null, 'transform', 'translate(.5 1.5)');
+            cardValueSvg.appendChild(cardValueSvgText);
+            cardGroup.appendChild(cardValueSvg);
+        }
+
+        return cardGroup;
+    }
+}
+
 class Svg {
 
     constructor(parent){
       console.log("SVG");
       this.svg = document.createElementNS(xmlns, "svg");
       this.svg.id = "svgArea";
-      this.svg.setAttribute("viewBox", "0 0 100 40"); 
+      this.svg.setAttribute("viewBox", "0 0 100 50"); 
 
       parent.append(this.svg);
     }
@@ -171,25 +248,44 @@ class Svg {
         console.log("SVG: drawTable()");
         console.log("pokertable: ", pokertable)
 
+        while(this.svg.firstChild) {    //reset SVG Area
+            this.svg.removeChild(this.svg.firstChild);
+        }
+
         this.playerTopGroup = document.createElementNS(xmlns, "g");
         this.playerTopGroup.id = "playerTop";
         this.playerTopGroup.setAttributeNS(null, 'transform', 'translate(50 5)');
 
         this.playerLeftGroup = document.createElementNS(xmlns, "g");
         this.playerLeftGroup.id = "playerLeft";
-        this.playerLeftGroup.setAttributeNS(null, 'transform', 'translate(10 20)');
+        this.playerLeftGroup.setAttributeNS(null, 'transform', 'translate(10 25)');
 
         this.playerRightGroup = document.createElementNS(xmlns, "g");
         this.playerRightGroup.id = "playerRight";
-        this.playerRightGroup.setAttributeNS(null, 'transform', 'translate(90 20)');
+        this.playerRightGroup.setAttributeNS(null, 'transform', 'translate(90 25)');
 
-        this.svg.append(this.playerTopGroup, this.playerLeftGroup, this.playerRightGroup);
+        this.playerSelfGroup = document.createElementNS(xmlns, "g");
+        this.playerSelfGroup.id = "playerSelf";
+        this.playerSelfGroup.setAttributeNS(null, 'transform', 'translate(50 40)');
+
+        this.svg.append(this.playerTopGroup, this.playerLeftGroup, this.playerRightGroup, this.playerSelfGroup);
 
         var ownPlayerID = pokertable.playerId;
         var players = [];   //just to be sure that array index correlates to player's id
         for(let player of pokertable.players) {
             players[player.id] = player;
         }
+
+
+        var ownPlayer = players[ownPlayerID];
+        if(ownPlayer) {
+            var p = new SelfPlayer(ownPlayer.username, ownPlayer.balance, ownPlayer.card0, ownPlayer.card1);
+            if(ownPlayer.card0 && ownPlayer.card1) {
+                this.playerSelfGroup.append(p.getCardGroup());
+            }
+            p.appendDetails(this.playerSelfGroup);
+        }
+
 
         var otherPlayer = players[ (ownPlayerID + 1) % 4 ];
         if( otherPlayer ) {    //left player
